c0af8d6c6dc5300fc91f18f347549d8458776bfa
Joseph Simbolon <119671425+JSKUN2@users.noreply.github.com>
2025-07-05 11:19:00 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,23 @@ import Top2 from './Top2/top2.jsx'
 import Middle from './Middle/middle.jsx'
 import Bottom from './Bottom/bottom.jsx'
 import './index.css'
+import { motion } from 'framer-motion';
 function App() {
   return (
-    <div className='overflow-x-hidden w-[100vw] mt-0 mb-0 pt-0 pd-0 font-montserrat bg-[white]'>
-      <Top1/>
-      <Top2/>
-      <Middle/>
-      <Bottom/>
-    </div>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.2 }}
+      viewport={{ once: true }}
+      className="min-h-screen bg-blue-100"
+    >
+      <div className='overflow-x-hidden w-[100vw] mt-0 mb-0 pt-0 pd-0 font-montserrat bg-[white]' data-scroll-section>
+        <Top1 />
+        <Top2 />
+        <Middle />
+        <Bottom />
+      </div>
+      </motion.div>
   )
 }
 
