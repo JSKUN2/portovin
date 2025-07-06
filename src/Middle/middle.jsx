5a0f0693a85ea07f6cc68f616df8bfb74c72b149
Joseph Simbolon <119671425+JSKUN2@users.noreply.github.com>
2025-07-07 00:35:07 +0700

    Update middle.jsx

--- src/Middle/middle.jsx
+++ src/Middle/middle.jsx
@@ -51,7 +51,7 @@ export default function Middle() {
             return (
               <SpotlightCard
                 key={index}
-                className={`custom-spotlight-card w-[480px] h-[240px] shrink-0 active text-[#FFF9F8]`}
+                className={`custom-spotlight-card w-[420px] h-[260px] shrink-0 active text-[#FFF9F8]`}
                 spotlightColor="#BAD1FF"
               >
                 {(
@@ -80,7 +80,7 @@ export default function Middle() {
             return (
               <SpotlightCard
                 key={index}
-                className={`custom-spotlight-card ${Mobile ? 'w-[260px] h-[132px]' : 'w-[480px] h-[240px]'} shrink-0 passive`}
+                className={`custom-spotlight-card w-[420px] h-[260px] shrink-0 passive`}
                 spotlightColor="#BAD1FF"
               />
             );
