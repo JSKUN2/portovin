1979286e4c046c9f05e236e7f11cc63fc9f4077d
Joseph Simbolon <119671425+JSKUN2@users.noreply.github.com>
2026-04-06 21:37:28 +0700

    Update head.jsx

--- src/components/head.jsx
+++ src/components/head.jsx
@@ -38,7 +38,7 @@ useEffect(() => {
     return () => mediaQuery.removeEventListener("change", handleValueChange);
 }, []);
     return(
-        <div className="max-md:h-[76vh] md:h-screen w-screen flex flex-col justify-center items-center pt-0 relative">
+        <div className="h-[100vh] w-screen flex flex-col justify-center items-center pt-0 relative">
               <StaggeredMenu
     position="right"
     items={menuItems}
@@ -57,7 +57,7 @@ useEffect(() => {
     scrollToProject={scrollToProject}
     active={active}
   />
-            <div className="flex w-[80vw] flex-row max-md:flex-col h-[360px] absolute top-88 left-1/2 -translate-x-1/2 -translate-y-1/2 justify-center items-center">
+            <div className="flex w-[80vw] flex-row max-md:flex-col h-[360px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 justify-around items-center">
                     <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0}>
                         <TiltedCard
                           imageSrc={PP}
@@ -81,7 +81,7 @@ useEffect(() => {
                     <p className="font-semibold text-[24px] text-black/40 leading-[32px] w-[40vw] max-md:w-[100%] max-md:text-[14px]">Creating stylized game worlds, characters, and intuitive digital experiences.</p>
                 </div></FadeContent>
             </div>
-                <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0} className="flex flex-row w-[90%] h-[64px] absolute bottom-0 justify-between">
+                <FadeContent blur={true} duration={1000} easing="ease-out" initialOpacity={0} className="flex flex-row w-[90%] h-[64px] justify-between absolute bottom-0">
                     <div className="text-[24px] font-semibold leading-[44px] flex flex-row gap-[20px] justify-center items-center text-center"><div className="text-[48px] max-md:text-[28px]"></div> <div className="mt-1 max-md:text-[16px]">Indonesia</div></div><div className="font-medium text-[16px] flex flex-col max-md:text-[16px] max-md:mt-2.5"><a href="https://www.instagram.com/glenndovin" className=" max-md:text-[14px]">Instagram</a><a href="https://www.linkedin.com/in/glenndovin-dherrel-junaidy-27a251349/" className="max-md:text-[14px]">Linkedin</a></div></FadeContent></div>
     )
 }
